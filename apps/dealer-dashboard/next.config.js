92a8b3867d553445b00b09968ce815ef23127cbc
AutoAgent Developer <developer@autoagent.com>
2025-12-04 12:32:49 -0500

    Fix webpack alias resolution for uvs-vehicles import

--- apps/dealer-dashboard/next.config.js
+++ apps/dealer-dashboard/next.config.js
@@ -41,7 +41,7 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
